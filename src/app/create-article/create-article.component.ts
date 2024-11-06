64a36eedeb4921a27fae9493242630179dd01b80
Ella Kuksa <beeella2804@gmail.com>
2024-11-06 22:45:40 +0100

    Implemented article-form

--- src/app/create-article/create-article.component.ts
+++ src/app/create-article/create-article.component.ts
@@ -8,4 +8,15 @@ import {ArticleFormComponent} from '../shared/components/article-form/article-fo
   templateUrl: './create-article.component.html',
   styleUrl: './create-article.component.scss',
 })
-export class CreateArticleComponent {}
+export class CreateArticleComponent {
+  initialValues = {
+    title: 'title',
+    description: 'description',
+    body: 'body',
+    tagList: ['dragon', 'cofee'],
+  };
+
+  onSubmit(res: any): void {
+    console.log(res);
+  }
+}
--- src/app/shared/components/article-form/article-form.component.ts
+++ src/app/shared/components/article-form/article-form.component.ts
@@ -1,17 +1,50 @@
-import {Component, Input} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 import {ArticleInputInterface} from '../../types/articleInput.interface';
 import {BackendErrorsInterface} from '../../types/backendErrors.interface';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {BackendErrorMsgsComponent} from '../backend-error-msgs/backend-error-msgs.component';
 
 @Component({
   selector: 'article-form',
   standalone: true,
-  imports: [],
+  imports: [BackendErrorMsgsComponent, ReactiveFormsModule],
   templateUrl: './article-form.component.html',
   styleUrl: './article-form.component.scss',
 })
-export class ArticleFormComponent {
+export class ArticleFormComponent implements OnInit {
   @Input('initialValues') initialValuesProps!: ArticleInputInterface;
   @Input('isSubmitting') isSubmittingProps!: boolean;
   @Input('errors') backendErrorsProps!: BackendErrorsInterface | null;
+
+  @Output('articleSubmit') articleSubmitEvent =
+    new EventEmitter<ArticleInputInterface>();
+
+  form!: FormGroup;
+
+  private readonly fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      title: this.initialValuesProps.title,
+      description: this.initialValuesProps.description,
+      body: this.initialValuesProps.body,
+      tagList: this.initialValuesProps.tagList.join(' '),
+    });
+  }
+
+  onSubmit(): void {
+    this.articleSubmitEvent.emit(this.form.value);
+  }
 }
